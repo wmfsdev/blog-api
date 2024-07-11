a47a7ab5ece8f40191d7fe6d51503e3aeaabbf6b
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-07-11 21:23:24 +0100

    rework config for JWT Strategy

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -11,19 +12,20 @@ passport.use(
     console.log('local strat');
     try {
       const user = await User.findOne({ username });
-      console.log('found user: ', user);
+      //  console.log('found user: ', user);
       if (!user) {
-        console.log('user');
+        //    console.log('user');
         return done(null, false, { message: 'Incorrect username' });
       }
       const match = await bcrypt.compare(password, user.password);
       if (!match) {
-        console.log('fail');
+        //    console.log('fail');
         return done(null, false, { message: 'Incorrect password' });
       }
-      console.log('user matches: ', user);
+      //   console.log('user matches: ', user);
       return done(null, user);
     } catch (err) {
+    //  console.log('bad login');
       return done(err);
     }
   }),
@@ -33,35 +35,16 @@ const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = 'your-256-bit-secret';
 
-passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-  console.log(jwt_payload);
-  if (jwt_payload) {
-    // sign
-  }
-  return done(null, jwt_payload);
-  // User.find({ id: jwt_payload.sub }, (err, user) => {
-  //   if (err) {
-  //     return done(err, false);
-  //   }
-  //   if (user) {
-  //     return done(null, user);
-  //   }
-  //   return done(null, false);
-  //   // or you could create a new account
-  // });
-}));
-
-// might not need when we swap to jwt
-
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser(async (id, done) => {
-//   try {
-//     const user = await User.findById(id);
-//     done(null, user);
-//   } catch (err) {
-//     done(err);
-//   }
-// });
+passport.use(
+  new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log('jwt strat');
+    console.log(jwt_payload);
+    try {
+      console.log('null');
+      return done(null, jwt_payload, { message: 'authorised' });
+    } catch (error) {
+      console.log('dsf');
+      done(error);
+    }
+  }),
+);

a85c0802ec7a2f2dcab3f528b4844c411747b165
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-07-14 23:34:06 +0100

    add new route handlers

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
 const router = express.Router();
+
 const indexController = require('../controllers/indexController');
+const articleController = require('../controllers/articleController');
 
 require('../config/passport');
 
 router.get('/', indexController.index);
+
+// ARTICLES
+
+router.post('/articles', articleController.article_post);
+
+router.get('/articles', articleController.article_get);
+
+// ELSE
 
 router.get('/signup', indexController.signup_get);
 
@@ -14,5 +24,7 @@ router.post('/signup', indexController.signup_post);
 router.get('/signin', indexController.signin_get);
 
 router.post('/signin', indexController.signin_post);
+
+router.get('/protected', indexController.protected);
 
 module.exports = router;
